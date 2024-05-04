4be022e42f1dc8f2ad70ee63da92a620c8a90b13
Jack-Tillman <johntillman345@gmail.com>
2024-05-04 18:34:53 -0400

    Finalized progressbar with text indications

--- client/src/components/ProgressBar.jsx
+++ client/src/components/ProgressBar.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import LinearProgress from '@mui/material/LinearProgress';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { useLoading } from '../contexts/LoadingContext';
 import "../styles/app.css";
 
 
@@ -23,16 +24,17 @@ LinearProgressWithLabel.propTypes = {
   value: PropTypes.number.isRequired,
 };
 
-export default function LinearWithValueLabel({ progress, isLoading }) {
+export default function LinearWithValueLabel({ progress }) {
+  const { isLoading } = useLoading();
 
   const getTextForProgress = (progress) => {
-    if (progress < 25 && isLoading) {
+    if (progress === 25) {
       return "Making your report now...";
-    } else if ((25 < progress < 51) && isLoading) {
+    } else if (progress === 50) {
       return "Converting report into appropriate file type...";
-    } else if ((51 < progress < 76) && isLoading) {
+    } else if (progress === 75) {
       return "Inserting report into the database...";
-    } else if ((76 < progress < 101) && isLoading) {
+    } else if (progress === 100) {
       return "Insertion complete!";
     } else {
       return "";
@@ -40,7 +42,7 @@ export default function LinearWithValueLabel({ progress, isLoading }) {
   };
 
   const progressText = getTextForProgress(progress);
-  const shouldDisplayProgress = progress > 0 && progress <= 100;
+  const shouldDisplayProgress = isLoading && progress > 0 && progress <= 100;
 
   return (
     <Box sx={{ width: '60%', margin: "0 auto", padding: "1rem" }}>
